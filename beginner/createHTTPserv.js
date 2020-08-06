81a2b3c98c003c8c8f0be1f002b8ef3b2eabeb9d
kyungim.lee <kyungim.lee@koscom.co.kr>
2020-08-06 16:40:29 +0900

    initial commit

---
+++ beginner/createHTTPserv.js
@@ -0,0 +1,12 @@
+/**
+ * Create HTTP Server in NodeJS
+ */
+var http = require("http");
+
+/**
+ * Hello, world 문자열을 반환하는 http 웹서버를 생성한다.
+ * 
+ */
+http.createServer(function(req, res) {
+
+})
---
+++ beginner/printHW.js
@@ -0,0 +1,4 @@
+/**
+ * Hello World in NodeJS
+ */
+console.log("Hello, world");
