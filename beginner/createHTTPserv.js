008f715b52e27b688910fab46aeb81270f2b67c5
kyungim.lee <kyungim.lee@koscom.co.kr>
2020-08-14 17:41:36 +0900

    20200814

--- beginner/createHTTPserv.js
+++ beginner/createHTTPserv.js
@@ -5,8 +5,14 @@ var http = require("http");
 
 /**
  * Hello, world 문자열을 반환하는 http 웹서버를 생성한다.
- * 
+ * 8081 포트로 설정한다.
  */
 http.createServer(function(req, res) {
+    
+    // 200 ok 응답 전송
+    res.writeHead(200, {'Content-Type': 'text/plain'});
 
-})
+    // response body message 설정
+    res.end('hello world');
+}).listen(8081);
+
